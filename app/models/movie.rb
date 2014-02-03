dafcb106adbdbbac31d927e14b9f65114aca7328
Arnaud Besnier <arnaudibesnier@gmail.com>
2014-02-04 00:51:53 +0100

    Add movie scopes

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -3,4 +3,7 @@ class Movie < ActiveRecord::Base
   attr_accessible :alias, :name, :poster, :release_date, :genre, :duration,
                   :synopsis, :director, :actors, :url_teaser, :url_playlist
 
+  scope :reviewed,   -> { where(reviewed: true) }
+  scope :unreviewed, -> { where(reviewed: false) }
+
 end
