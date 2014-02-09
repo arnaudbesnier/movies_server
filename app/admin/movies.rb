910150e09a487db4fcd90ede863201157dd42bf8
Arnaud Besnier <arnaudibesnier@gmail.com>
2014-02-09 02:24:27 +0100

    Improve the form for movie validation

--- app/admin/movies.rb
+++ app/admin/movies.rb
@@ -20,32 +20,6 @@ ActiveAdmin.register Movie do
     column('') { |movie| edit_link_icon(movie) }
   end
 
-  form do |f|
-    f.inputs 'story' do
-      f.input :alias
-      f.input :name
-      f.input :genre
-      f.input :synopsis
-    end
-
-    f.inputs 'infos' do
-      f.input :director
-      f.input :actors
-      f.input :duration
-      f.input :release_date
-    end
-
-    f.inputs 'links' do
-      f.input :poster
-      f.input :url_teaser
-      f.input :url_playlist
-    end
-
-    f.inputs 'review' do
-      f.input :reviewed
-    end
-
-    f.buttons
-  end
+  form partial: 'admin/movies/edit/general'
 
 end
