fef057cd2c6f3bcb8e9437db02a99594918e1427
Arnaud Besnier <arnaudibesnier@gmail.com>
2014-03-02 20:57:15 +0100

    Add reviewed as attr_accessible

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,7 +1,7 @@
 class Movie < ActiveRecord::Base
 
   attr_accessible :alias, :name, :poster, :release_date, :genre, :duration,
-                  :synopsis, :director, :actors, :url_teaser, :url_playlist
+                  :synopsis, :director, :actors, :url_teaser, :url_playlist, :reviewed
 
   scope :reviewed,   -> { where(reviewed: true) }
   scope :unreviewed, -> { where(reviewed: false) }
