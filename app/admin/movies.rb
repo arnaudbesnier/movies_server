64fb186d4ea6c95c9e49a4b4610f14a7df9a5c09
Arnaud Besnier <arnaudibesnier@gmail.com>
2014-03-12 13:49:39 +0100

    Improve movie form

--- app/admin/movies.rb
+++ app/admin/movies.rb
@@ -5,6 +5,29 @@ ActiveAdmin.register Movie do
 
   actions :index, :edit, :update
 
+  controller do
+
+    def update
+      update_from_button = params[:commit]
+      validate_action = (update_from_button == 'Update & Validate')
+
+      params[:movie][:reviewed] = true if validate_action
+
+      update! do |format|
+        if @movie.valid?
+          if validate_action
+            flash[:notice] = 'Movie was successfully validated.'
+            format.html { redirect_to admin_movies_path }
+          else
+            flash[:notice] = 'Movie was successfully updated.'
+            format.html { redirect_to edit_admin_movie_path(@movie) }
+          end
+        end
+      end
+    end
+
+  end
+
   scope :reviewed
   scope :unreviewed, default: true
 
